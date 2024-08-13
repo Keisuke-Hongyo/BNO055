0007d5f03a1f7e89053b67dd4745e4467b9fd077
本行　圭介 <kei.hongyo@gmail.com>
2024-08-13 11:55:24 +0900

    サンプルプログラム修正

--- main.go
+++ main.go
@@ -18,7 +18,8 @@ import (
 
 func main() {
 	var chk bool
-	//var roll, pich, yaw float64
+	var roll, pich, yaw float64
+	var proc uint8
 	machine.I2C0.Configure(machine.I2CConfig{
 		Frequency: 400 * machine.KHz,
 	})
@@ -29,52 +30,75 @@ func main() {
 	d := bno055.New(machine.I2C0)
 
 	if d.Init() {
+		proc = 6
 		for {
 			led.Low()
 			time.Sleep(time.Millisecond * 50)
-			/*chk = d.GetAccl()
-			if !chk {
-				println("Error")
-			} else {
-				fmt.Printf("Accl xData=%d, Ydata=%d, Zdata=%d \n",
-					d.SensorData.AcclData.XData, d.SensorData.AcclData.YData, d.SensorData.AcclData.ZData)
-			}*/
-			/*chk = d.GetGyro()
-			if !chk {
-				println("Error")
-			} else {
-				fmt.Printf("Gyro xData=%d, Ydata=%d, Zdata=%d \n",
-					d.SensorData.GyroData.XData, d.SensorData.GyroData.YData, d.SensorData.GyroData.ZData)
-			}*/
-			chk = d.GetMag()
-			if !chk {
-				println("Error")
-			} else {
-				fmt.Printf("Mag xData=%d, Ydata=%d, Zdata=%d \n",
-					d.SensorData.MagData.XData, d.SensorData.MagData.YData, d.SensorData.MagData.ZData)
-			}
-			/*chk = d.GetQuaternion()
-			if !chk {
-				println("Error")
-			} else {
-				fmt.Printf("Quaternion wData=%d, xData=%d, Ydata=%d, Zdata=%d \n",
-					d.SensorData.QutaData.WData, d.SensorData.QutaData.XData,
-					d.SensorData.QutaData.YData, d.SensorData.QutaData.ZData,
-				)
-			}*/
-			/*_, temp := d.GetTemp()
-			time.Sleep(time.Millisecond * 10)
-
-			for {
-				chk, roll, pich, yaw = d.QuaternionToEuler()
-				if chk {
-					break
+			switch proc {
+			case 1:
+				chk = d.GetAccl()
+				if !chk {
+					println("Error")
+				} else {
+					fmt.Printf("Accl xData=%d, Ydata=%d, Zdata=%d \n",
+						d.SensorData.AcclData.XData, d.SensorData.AcclData.YData, d.SensorData.AcclData.ZData)
 				}
-				time.Sleep(time.Millisecond * 100)
+				break
+			case 2:
+				chk = d.GetGyro()
+				if !chk {
+					println("Error")
+				} else {
+					fmt.Printf("Gyro xData=%d, Ydata=%d, Zdata=%d \n",
+						d.SensorData.GyroData.XData, d.SensorData.GyroData.YData, d.SensorData.GyroData.ZData)
+				}
+				break
+			case 3:
+				chk = d.GetMag()
+				if !chk {
+					println("Error")
+				} else {
+					fmt.Printf("Mag xData=%d, Ydata=%d, Zdata=%d \n",
+						d.SensorData.MagData.XData, d.SensorData.MagData.YData, d.SensorData.MagData.ZData)
+				}
+				break
+			case 4:
+				chk = d.GetEuler()
+				if !chk {
+					println("Error")
+				} else {
+					fmt.Printf("Quaternion RData=%d, Pdata=%d, Hdata=%d \n",
+						d.SensorData.EulerData.RData, d.SensorData.EulerData.PData, d.SensorData.EulerData.HData,
+					)
+				}
+				break
+			case 5:
+				chk = d.GetQuaternion()
+				if !chk {
+					println("Error")
+				} else {
+					fmt.Printf("Quaternion wData=%d, xData=%d, Ydata=%d, Zdata=%d \n",
+						d.SensorData.QutaData.WData, d.SensorData.QutaData.XData,
+						d.SensorData.QutaData.YData, d.SensorData.QutaData.ZData,
+					)
+				}
+				break
+			case 6:
+				_, temp := d.GetTemp()
+				time.Sleep(time.Millisecond * 10)
+
+				for {
+					chk, roll, pich, yaw = d.QuaternionToEuler()
+					if chk {
+						break
+					}
+					time.Sleep(time.Millisecond * 100)
+				}
+
+				fmt.Printf("Euler temp=%d roll=%f, pich=%f, yaw=%f \n", temp, roll, pich, yaw)
+				break
 			}
 
-			fmt.Printf("Euler temp=%d roll=%f, pich=%f, yaw=%f \n", temp, roll, pich, yaw)*/
-			//fmt.Printf("Euler roll=%f, pich=%f, yaw=%f \n", roll, pich, yaw)
 			led.High()
 			time.Sleep(time.Millisecond * 50)
 		}
